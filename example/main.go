b9a70d1d8f2fbab5ab7768794770d503a6584ac7
bjdgyc <bjdgyc@163.com>
2016-07-05 18:31:05 +0800

    更新说明文件

--- example/main.go
+++ example/main.go
@@ -1,25 +1,3 @@
-# slog
-
-## Introduction
-
-simple log
-简单日志是一个用golang编写的简单日志类
-
-该类库提供了`FATAL、ERROR、WARN、INFO、DEBUG` 五个日志等级
-
-同时提供了一个记录访问日志的接口 `SetAccessFile(name, accessFile string)`
-该接口可根据不同的业务写入不同的文件
-该日志文件内部实现了按天切割
-
-## Installation
-
-`go get github.com/bjdgyc/slog`
-
-
-## example
-
-```go
-
 package main
 
 import "github.com/bjdgyc/slog"
@@ -39,9 +17,3 @@ func main() {
 	//LogRecord: 2016/07/05 01:28:34 订单信息
 	slog.Access("order", "订单信息")
 }
-
-
-```
-
-
-
